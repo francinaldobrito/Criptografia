e448adbf3bdbdc78f58edec55ed90535e1d71d75
Francinaldo Brito <nardinbrito@gmail.com>
2019-10-06 16:55:23 -0300

    consertando script force brute

--- cifra de Cesar Brute force.py
+++ cifra de Cesar Brute force.py
@@ -1,14 +1,15 @@
 import	sys
 from	string	import	lowercase	as	lc
 file	=	open(sys.argv[1],'r').read().lower()
+
 for	key	in	xrange(1,26):
-    result	=	''
+    result	= ''
     print	'Key:',	key
     for	lt	in	file:
         if	lt	in	lc:
             idx	=	lc.find(lt)
-            idx	=	(idx	- key)	%	26
+            idx	=	(idx - key) % 26
             result	+=	lc[idx]
         else:
             result	+=	lt
-print	result
+    print result,
